4da56a6d47a36dc3e4b0a3635dfe0a6bf3cb4d3a
scybak <scybak@auxiliumgroup.com>
2018-04-03 13:10:20 -0400

    Init

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import test from './test.html';
+
+const node = document.createElement('div');
+node.innerHTML = test;
+document.body.appendChild(node);
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+	  },
+	  {
+		test: /\.html$/,
+		exclude: '/node_modules/',
+		use: 'html-loader?exportAsEs6Default'
+	}
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
