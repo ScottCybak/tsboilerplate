d87af54d5538656eccfaab07c3c4ea65b33b7f57
scybak <scybak@auxiliumgroup.com>
2018-04-20 14:06:42 -0400

    adding rxjs to package, with ghetto test

--- src/test/test.ts
+++ src/test/test.ts
@@ -2,7 +2,10 @@ import testHtml from './test.html';
 import testCss from './test.css';
 import testJson from './test.json';
 
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
 export function test() {
+
 	// make sure our html file was loaded
 	const node = document.createElement('div');
 	node.innerHTML = testHtml;
@@ -21,4 +24,32 @@ export function test() {
 	// try to coerce the json into a defined schema...
 	const definedJson: {foo: string, bar: any[]} = testJson;
 	console.log({definedJson});
+
+	// lets test our observable stuffs
+	const delay = 100,
+		subject = new BehaviorSubject<boolean>(false),
+		subscriber = subject.subscribe(
+			(status) => {
+				console.log('status change', status);
+			},
+			(err) => {
+				console.log('error', err);
+			},
+			() => {
+				console.log('complete!'); // cant seem to reach this..
+			}
+		);
+
+	setTimeout(() => subject.next(true), delay * 1);
+	setTimeout(() => subject.error('intentional error'), delay * 2);
+	// setTimeout(() => subject.complete(), 1000);
+	setTimeout(() => subscriber.unsubscribe(), delay * 3);
+	setTimeout(() => {
+		if (subject.observers.length) {
+			console.warn('Subject wasn\'t unsubscribed property...');
+		} else {
+			console.log('Unsubscribe was successful!');
+		}
+	}, delay * 4)
+
 }
